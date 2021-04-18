f3328d10aa930e0ad15a65456e27aaa9a814ed03
swethuvasi28 <79217442+swethuvasi28@users.noreply.github.com>
2021-04-18 10:10:35 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout << "Enter your age:" ;
+	cin >> age;
+	
+	if(age<18) {
+		cout << " oh!! noo you wanna !! grow ....";
+		}
+	else if(age==18)
+	{
+		cout << "you are 18+ now";
+	}
+	else{
+	
+		cout << "Better you can get votter ID";
+		
+		
+	}
+	return 0;
+}
